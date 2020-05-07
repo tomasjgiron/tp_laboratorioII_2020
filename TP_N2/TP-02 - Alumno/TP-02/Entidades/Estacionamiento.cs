f503fece8abf9ef941c247bbc9ace12562a5bfc3
Tomás Girón <tomas.j.giron@gmail.com>
2020-05-07 02:00:46 -0300

    Estacionamiento Alumno

---
+++ TP_N2/TP-02 - Alumno/TP-02/Entidades/Estacionamiento.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// No podrá tener clases heredadas.
+    /// </summary>
+    public sealed class Estacionamiento
+    {
+        private List<Vehiculo> vehiculos;
+        private int espacioDisponible;
+        public enum ETipo
+        {
+            Moto, Automovil, Camioneta, Todos
+        }
+
+        #region "Constructores"
+        private Estacionamiento()
+        {
+            this.vehiculos = new List<Vehiculo>();
+        }
+        public Estacionamiento(int espacioDisponible) :this()
+        {
+            this.espacioDisponible = espacioDisponible;
+        }
+        #endregion
+
+        #region "Sobrecargas"
+        /// <summary>
+        /// Muestro el estacionamiento y TODOS los vehículos
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return this.Mostrar(this, ETipo.Todos);
+        }
+        #endregion
+
+        #region "Métodos"
+
+        /// <summary>
+        /// Expone los datos del elemento y su lista (incluidas sus herencias)
+        /// SOLO del tipo requerido
+        /// </summary>
+        /// <param name="c">Elemento a exponer</param>
+        /// <param name="ETipo">Tipos de ítems de la lista a mostrar</param>
+        /// <returns></returns>
+        public string Mostrar(Estacionamiento c, ETipo tipo)
+        {
+            StringBuilder cadenaEstacionamiento = new StringBuilder();
+
+            cadenaEstacionamiento.AppendFormat($"Tenemos {c.vehiculos.Count} lugares ocupados " +
+                $"de un total de {c.espacioDisponible} disponibles");
+            cadenaEstacionamiento.AppendLine("");
+            foreach (Vehiculo v in c.vehiculos)
+            {
+                switch (tipo)
+                {
+                    case ETipo.Camioneta:
+                        if (v is Camioneta)
+                        {
+                            cadenaEstacionamiento.AppendLine(v.Mostrar());
+                        }
+                        break;
+
+                    case ETipo.Moto:
+                        if (v is Moto)
+                        {
+                            cadenaEstacionamiento.AppendLine(v.Mostrar());
+                        }
+                        break;
+
+                    case ETipo.Automovil:
+                        if (v is Automovil)
+                        {
+                            cadenaEstacionamiento.AppendLine(v.Mostrar());
+                        }
+                        break;
+
+                    default:
+                        cadenaEstacionamiento.AppendLine(v.Mostrar());
+                        break;
+                }
+            }
+            return cadenaEstacionamiento.ToString();
+        }
+        #endregion
+
+        #region "Operadores"
+        /// <summary>
+        /// Agregará un elemento a la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se agregará el elemento</param>
+        /// <param name="p">Objeto a agregar</param>
+        /// <returns></returns>
+        public static Estacionamiento operator +(Estacionamiento c, Vehiculo p)
+        {
+            foreach (Vehiculo v in c.vehiculos)
+            {
+                if (v == p)
+                {
+                    return c;
+                }
+            }
+            if(c.vehiculos.Count < c.espacioDisponible)
+            {
+                c.vehiculos.Add(p);
+            }
+            return c;
+        }
+        /// <summary>
+        /// Quitará un elemento de la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se quitará el elemento</param>
+        /// <param name="p">Objeto a quitar</param>
+        /// <returns></returns>
+        public static Estacionamiento operator -(Estacionamiento c, Vehiculo p)
+        {
+            foreach (Vehiculo v in c.vehiculos)
+            {
+                if (v == p)
+                {
+                    c.vehiculos.Remove(v);
+                    break;
+                }
+            }
+            return c;
+        }
+        #endregion
+    }
+}
---
+++ TP_N2/TP-02 - Alumno/TP-02/Entidades/Moto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Moto : Vehiculo
+    {
+        public Moto(EMarca marca, string codigo, ConsoleColor color) 
+            :base(marca, codigo, color)
+        {
+        }
+
+        /// <summary>
+        /// Las motos son chicas
+        /// </summary>
+        protected override ETamanio Tamanio 
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder cadenaMoto = new StringBuilder();
+
+            cadenaMoto.AppendLine("MOTO");
+            cadenaMoto.AppendLine(base.Mostrar());
+            cadenaMoto.AppendLine($"Tamaño: {this.Tamanio}");
+            cadenaMoto.AppendLine("");
+            cadenaMoto.AppendLine("---------------------");
+
+            return cadenaMoto.ToString();
+        }
+    }
+}
